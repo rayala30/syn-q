46730bd46c353a31e1091efd91b5cbad96ebf59c
Rafael Ayala <rafayala30@gmail.com>
2025-02-02 20:48:12 -0500

    db connection debug check

--- app/config.py
+++ app/config.py
@@ -1,9 +1,8 @@
 import os
 
-import os
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') # Use Railway environment variable
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/synq_db")
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)  # Generate a random key if not set
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # Use Railway's DATABASE_URL
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # Fetch DATABASE_URL from Railway environment
 
