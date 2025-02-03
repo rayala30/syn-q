67b1c170fed1dd03afd8352c8d630197dfcfa880
Rafael Ayala <rafayala30@gmail.com>
2025-02-02 21:54:22 -0500

    db connection debug environment

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ import os
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/synq_db")
+    # Log all environment variables
+    print(f"All environment variables: {os.environ}")
+
     DATABASE_URL_1 = os.environ.get("DATABASE_URL_1")
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)  # Generate a random key if not set
     SQLALCHEMY_TRACK_MODIFICATIONS = False
