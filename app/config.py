060a5c597d8a0ae53e45bb4dd60041465c29a69d
Rafael Ayala <rafayala30@gmail.com>
2025-02-02 21:14:18 -0500

    db url connection tester

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/synq_db")
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)  # Generate a random key if not set
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://postgres:SbNVVTiMwUUZBgndfKFbRvtKrsOpwMCr@postgres.railway.internal:5432/railway")  # Fetch DATABASE_URL from Railway environment
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:SbNVVTiMwUUZBgndfKFbRvtKrsOpwMCr@postgres.railway.internal:5432/railway")  # Fetch DATABASE_URL from Railway environment
 
