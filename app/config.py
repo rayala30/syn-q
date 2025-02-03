ad864ae20e72ab8a2064086d2d5cf4d95fd7cada
Rafael Ayala <rafayala30@gmail.com>
2025-02-02 21:47:56 -0500

    db connection debug

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/synq_db")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL_1 = os.environ.get("DATABASE_URL_1")
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)  # Generate a random key if not set
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    print(f"DATABASE_URL: {DATABASE_URL}")  # Add this line to debug
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL or "postgresql://localhost/synq_db"  # Default fallback
+    print(f"DATABASE_URL: {DATABASE_URL_1}")  # Add this line to debug
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL_1 or "postgresql://localhost/synq_db"  # Default fallback
 
