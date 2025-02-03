a3595438d9d82ae46e589892ebe40f1756ddd6d8
Rafael Ayala <rafayala30@gmail.com>
2025-02-02 21:05:00 -0500

    db connection debug init checks

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from app.config import Config
 
 db = SQLAlchemy()
 
@@ -8,7 +9,8 @@ def create_app():
     app = Flask(__name__)
 
     # Load the config file
-    app.config.from_object("app.config.Config")
+    # app.config.from_object("app.config.Config")
+    app.config.from_object(Config)
 
     # Initialize the app with db
     db.init_app(app)
